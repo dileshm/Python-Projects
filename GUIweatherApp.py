f8f38ffebc58b271468f5d4698e7d8ef8e81bfc8
Dileshm <dileshmakanjee@hotmail.com>
2024-08-29 23:06:15 +0100

    Update

---
+++ GUIweatherApp.py
@@ -0,0 +1,8 @@
+import sys
+import requests
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
+                             QLineEdit, QPushButton, QVBoxLayout)
+from PyQt5.QtCore import Qt
+
+
+class WeatherApp(QWidget):
