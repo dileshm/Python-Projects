c4459654e07570994c45ce1b28091b101feabc04
Dileshm <dileshmakanjee@hotmail.com>
2024-08-30 08:36:06 +0100

    Update

--- GUIweatherApp.py
+++ GUIweatherApp.py
@@ -6,3 +6,18 @@ from PyQt5.QtCore import Qt
 
 
 class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enter city name: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel("30°C")
+        self.weather_indicator = QLabel("☀️", self)
+        self.description_label = QLabel("Sunny", self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
