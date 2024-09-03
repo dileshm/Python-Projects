e9d138a725fc8a56f6fbac8830c943e800441dee
Dileshm <dileshmakanjee@hotmail.com>
2024-09-03 19:57:45 +0100

    JPGtoPNG

---
+++ JPGtoPNGconverter/JPGtoPNG.py
@@ -0,0 +1,15 @@
+import sys
+import os
+from PIL import Image
+
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f"{image_folder}{filename}")
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f"{output_folder}{clean_name}.png", "png")
+    print("Image Converted!")
