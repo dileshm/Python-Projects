c78a15178d9459593667a6dc1a6ecd41c4584afb
Dileshm <dileshmakanjee@hotmail.com>
2024-09-05 23:10:53 +0100

    Update

--- APIWeatherApp/WeatherApp.py
+++ APIWeatherApp/WeatherApp.py
@@ -3,6 +3,7 @@ import requests
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
                              QLineEdit, QPushButton, QVBoxLayout)
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
 
 
 class WeatherApp(QWidget):
@@ -18,6 +19,7 @@ class WeatherApp(QWidget):
 
     def initUI(self):
         self.setWindowTitle("Weather App")
+        self.setWindowIcon(QIcon("APIWeatherApp\weather_icon.png"))
 
         vbox = QVBoxLayout()
 
@@ -62,8 +64,9 @@ class WeatherApp(QWidget):
                 font-size: 75px;              
             }
             QLabel#weather_indicator{
-                font-size: 100px;
-                font-family: Segoe UI emoji;
+                font-size: 120px;
+                font-family: Segoe UI Emoji;
+                padding: 10px;
             }
             QLabel#description_label{
                 font-size: 50px;          
@@ -85,32 +88,80 @@ class WeatherApp(QWidget):
         except requests.exceptions.HTTPError as http_error:
             match response.status_code:
                 case 400:
-                    print("Bad Request\nPlease check your input")
+                    self.display_error("Bad Request:\nPlease check your input")
                 case 401:
-                    print("Unauthorised\nInvalid API Key")
+                    self.display_error("Unauthorised:\nInvalid API Key")
                 case 403:
-                    print("Forbidden\nAccess denied")
+                    self.display_error("Forbidden:\nAccess denied")
                 case 404:
-                    print("Not Found\nCity not found")
+                    self.display_error("Not Found:\nCity not found")
                 case 500:
-                    print("Internal Server Error\nPlease try again later")
+                    self.display_error(
+                        "Internal Server Error:\nPlease try again later")
                 case 502:
-                    print("Bad Gateway\nInvalid response from the server")
+                    self.display_error(
+                        "Bad Gateway:\nInvalid response from the server")
                 case 503:
-                    print("Service Unavailable\nServer is down")
+                    self.display_error("Service Unavailable:\nServer is down")
                 case 504:
-                    print("Gateway Timeout\nNo response from the server")
+                    self.display_error(
+                        "Gateway Timeout:\nNo response from the server")
                 case _:
-                    print(f"HTTP error occured\n{http_error}")
+                    self.display_error(f"HTTP error occured:\n{http_error}")
 
-        except requests.exceptions.RequestException:
-            pass
+        except requests.exceptions.ConnectionError:
+            self.display_error(
+                "Connection Error:\nPlease check your internet connection")
+        except requests.exceptions.Timeout:
+            self.display_error("Timeout Error:\nThe request timed out")
+        except requests.exceptions.TooManyRedirects:
+            self.display_error("Too Many Redirects:\nPlease check the URL")
+        except requests.exceptions.RequestException as req_error:
+            self.display_error(f"Request Error:\n{req_error}")
 
     def display_error(self, message):
-        pass
+        self.temperature_label.setStyleSheet("font-size: 30px")
+        self.temperature_label.setText(message)
+        self.weather_indicator.clear()
+        self.description_label.clear()
 
     def display_weather(self, data):
-        print(data)
+        self.temperature_label.setStyleSheet("font-size: 75px")
+        temperature_k = data["main"]["temp"]
+        temperature_c = temperature_k - 273.15
+        temperature_f = (temperature_k * 9/5) - 459.67
+        weather_id = data["weather"][0]["id"]
+        weather_description = data["weather"][0]["description"]
+
+        self.temperature_label.setText(f"{temperature_c:.0f}°C")
+        self.weather_indicator.setText(self.get_weather_indicator(weather_id))
+        self.description_label.setText(weather_description)
+
+    @staticmethod
+    def get_weather_indicator(weather_id):
+
+        if 200 <= weather_id <= 232:
+            return "⛈"
+        elif 300 <= weather_id <= 321:
+            return "🌦"
+        elif 500 <= weather_id <= 531:
+            return "🌧"
+        elif 600 <= weather_id <= 622:
+            return "🌨"
+        elif 701 <= weather_id <= 741:
+            return "🌫"
+        elif weather_id == 762:
+            return "🌋"
+        elif weather_id == 771:
+            return "💨"
+        elif weather_id == 781:
+            return "🌪"
+        elif weather_id == 800:
+            return "☀️"
+        elif 801 <= weather_id <= 804:
+            return "☁︎"
+        else:
+            return ""
 
 
 if __name__ == "__main__":
