f085dba4f992744855be7fdeca50eb94fb412c3a
Dileshm <dileshmakanjee@hotmail.com>
2024-09-17 00:29:47 +0100

    Update

--- MessengerApp/Server.py
+++ MessengerApp/Server.py
@@ -1,3 +1,34 @@
 import socket
-from _thread import *
-import sys
+import select
+
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockpt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+
+def recieve_messsage(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode("utf-8").strip())
+        return {"header": message_header, "data": client_socket.recv(message_length)}
+
+    except:
+        return False
+
+
+while True:
+    read_sockets, _, exception_sockets = select.select(sockets_list), [], sockets_list)
