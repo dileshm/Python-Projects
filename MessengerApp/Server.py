afc2d8b989dd45d2deccaf61256f111637e7e86f
Dileshm <dileshmakanjee@hotmail.com>
2024-09-17 23:53:43 +0100

    Update

--- MessengerApp/Server.py
+++ MessengerApp/Server.py
@@ -31,4 +31,26 @@ def recieve_messsage(client_socket):
 
 
 while True:
-    read_sockets, _, exception_sockets = select.select(sockets_list), [], sockets_list)
+    read_sockets, _, exception_sockets = select.select(
+        sockets_list, [], sockets_list)
+
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+
+            user = recieve_messsage(client_socket)
+            if user is False:
+                continue
+
+            sockets_list.append(client_socket)
+
+            clients[client_socket] = user
+
+            print(f"Accepted new connection from {client_address[0]}:{
+                  client_address[1]} username: {user["data"].decode("utf-8")}")
+
+        else:
+            messsage = recieve_messsage(notified_socket)
+
+            if message is false:
+                print(f"Closed connection from {client})
